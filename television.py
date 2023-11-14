307c74f36ed6e2076084b66cdff054731580be9f
DanielHODonnell <danielodonnell@unomaha.edu>
2023-11-14 08:05:28 -0600

    First File

--- television.py
+++ television.py
@@ -1,18 +1,33 @@
 class Television:
     MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
 
     def __init__(self):
         self.__muted = False
+        self.__status = False
+        self.__channel = Television.MIN_CHANNEL
         self.__volume = Television.MIN_VOLUME
 
     def power(self):
-        pass
+        if not self.__status:
+            self.__status = True
+        else:
+            self.__status = False
 
-    def mute(self):
-        pass
+    def mute(self) -> None:
+        if not self.__muted:
+            self.__muted = True
+        else:
+            self.__muted = False
 
     def channel_up(self):
-        pass
+        if self.__status:
+            if self.__status > Television.MIN_VOLUME:
+                self.__channel += 1
+            else:
+                self.__channel = Television.MAX_VOLUME
 
     def channel_down(self):
         if self.__status:
@@ -24,14 +39,17 @@ class Television:
     def volume_up(self):
         if self.__status:
             self.__muted = False
-            if self.__volume < Television.MAX_VOLUME
+            if self.__volume < Television.MAX_VOLUME:
                 self.__volume += 1
 
     def volume_down(self):
-        pass
+        if self.__status:
+            self.__muted = False
+            if self.__volume < Television.MAX_VOLUME:
+                self.__volume -= 1
 
-    def __str__(self):
+    def __str__(self) -> str:
         if self.__muted:
-            return f'Volue = {Television.MIN_VOLUME}'
+            return f'Volume = {Television.MIN_VOLUME}'
         else:
-            return f'xxx'
+            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
---
+++ test_television.py
@@ -0,0 +1,4 @@
+import pytest
+from television import *
+
+# class Test:
